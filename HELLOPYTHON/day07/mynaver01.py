fa9544d4cd51c6216f2f824bda56becf1c48ee45
maskan19 <maskan_19@naver.com>
2021-03-17 19:35:31 +0900

    no message

--- HELLOPYTHON/day07/mynaver01.py
+++ HELLOPYTHON/day07/mynaver01.py
@@ -33,8 +33,7 @@ else:
     # print(item['title'], 
           # item['href'][item['href'].find('/Stock/')+7:item['href'].find('_blank')-3])
     # print(item.find_parents('.tbody').children('dd'))
-    
-for info in soup.select('.tbody'):
-    print(info.dt.text, end="\t\t")
-    print(info.dd['id'][len(info.dd['id']) - 6:], end="\t\t")
-    print(info.dd.span.text)
+# for info in soup.select('.tbody'):
+    # print(info.dt.text, end="\t\t")
+    # print(info.dd['id'][len(info.dd['id']) - 6:], end="\t\t")
+    # print(info.dd.span.text)
